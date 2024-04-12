a72e25a9e6f69f8ae6f0b6c9d8615477096a637f
duyhung753951 <148344213+duyhung753951@users.noreply.github.com>
2024-04-12 10:25:15 +0700

    Update Bai_2.c

--- Bai_2.c
+++ Bai_2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 //ma tran m hang n cot
 void input_matrix(int m, int n, float a[][100]);
-void print_matrix(int m, int n, float a[][100]);
+void print_matrix(int m, int n, float a[][100], char name);
 void sum_matrix(int m, int n, float a[][100], float b[][100]);
 void multiply_matri(int m, int n, int p, float a[][100], float b[][100]);
 int main(){
@@ -20,10 +20,10 @@ int main(){
     printf("Nhap ma tran B: \n");
     input_matrix(m, n, b);
     printf("Ma tran A: \n");
-    print_matrix(m, n, a);
+    print_matrix(m, n, a, 'a');
     printf("Ma tran B: \n");
-    print_matrix(m, n, b);
-    printf("Ma tran C: \n");
+    print_matrix(m, n, b, 'b');
+    printf("Ma tran C(A+B): \n");
     sum_matrix(m, n, a, b);
 
     float d[100][100], e[100][100];
@@ -33,22 +33,22 @@ int main(){
     printf("Nhap ma tran D: \n");
     input_matrix(m, n, d);
     printf("Ma tran D: \n");
-    print_matrix(m, n, d);
+    print_matrix(m, n, d, 'd');
 
     printf("Nhap ma tran E: \n");
     input_matrix(n, p, e);
     printf("Ma tran E: \n");
-    print_matrix(n, p, e);
+    print_matrix(n, p, e, 'e');
 
     multiply_matri(m, n, p, d, e);
     return 0;
 }
 
-void input_matrix(int m, int n, float a[][100]){
+void input_matrix(int m, int n, float a[][100], char name){
     int i, j;
     for(i = 1; i <= m; i++){
         for(j = 1; j <= n; j++){
-            printf("[%d][%d] = ", i, j);
+            printf("%c[%d][%d] = ",name, i, j);
             scanf("%f", &a[i][j]);
         }
     }
@@ -84,10 +84,9 @@ void multiply_matri(int m, int n, int p, float a[][100], float b[][100])
         for(j = 1; j <= p; j++){
             for(z = 1; z <= n; z++){
                 f[i][j] += a[i][z] * b[z][j];
-                printf("f[%d][%d] = %f \n", i, j, f[i][j]);
             }
         }
     }
-    printf("Ma tran F: \n");
-    print_matrix(m, p, f);
+    printf("Ma tran F(DxE): \n");
+    print_matrix(m, p, f, 'f');
 }
